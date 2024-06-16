aa202e6410dc52b6f6b36ee5a6c54d302ad1cf30
Aditya kumar singh <142435507+Axestein@users.noreply.github.com>
2024-06-16 19:48:37 +0530

    Add files via upload

---
+++ birthdayCard/script.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+
+title.innerText = "happy birthday ayuraja";
+
+desc.innerText = "many many happy returns of the day and may god bless you.";
+
+
